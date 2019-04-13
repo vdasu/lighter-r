7b398a6514d2eefd62357a13e0d4038780869eff
anubhab001 <49355634+anubhab001@users.noreply.github.com>
2019-04-14 01:49:54 +0800

    tfc functionality being incorporated

--- tfc_files/lighter_to_tfc.py
+++ tfc_files/lighter_to_tfc.py
@@ -1,3 +1,9 @@
 
-# Compile with Python 2
-# Functionality to convert the output of an implementation file which is output from LIGHTER (in C-like syntax) to a TFC file
+# Run with Python 2
+# Functionality to convert the output of an implementation file which is output from LIGHTER (in C-like syntax) to a TFC [http://webhome.cs.uvic.ca/~dmaslov/mach-read.html] file
+
+file_name = 'test.c'
+impl_file = open(file_name, 'r')
+tfc_file = open(impl_file.split('.')[0]+'tfc', 'w')
+
+print >> tfc_file, 
